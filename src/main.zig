8b32f9b949cd1fb3e34b0aee13cd684f248897f1
Aleksander Długosz <olekdlugi@gmail.com>
2025-02-24 21:26:31 +0100

    Added HRTF & binaural effect

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 
 const c = @cImport({
-    // @cDefine("SDL_DISABLE_OLD_NAMES", {});
     @cInclude("phonon.h");
 });
 
@@ -14,4 +13,30 @@ pub fn main() !void {
     if(c.iplContextCreate(&context_settings, &context) != c.IPL_STATUS_SUCCESS) {
         return error.ContextCreationFailed;
     }
+
+    var hrtf_settings = c.IPLHRTFSettings {
+        .type = c.IPL_HRTFTYPE_DEFAULT,
+        .volume = 1,
+    };
+
+    var audio_settings = c.IPLAudioSettings {
+        .samplingRate = 44100,
+        .frameSize = 1024,
+    };
+
+    var hrtf: c.IPLHRTF = null;
+    if(c.iplHRTFCreate(context, &audio_settings, &hrtf_settings, &hrtf) != c.IPL_STATUS_SUCCESS) {
+        return error.HRTFCreationFailed;
+    }
+
+    var binaural_settings = c.IPLBinauralEffectSettings {
+        .hrtf = hrtf,
+    };
+
+    var binaural: c.IPLBinauralEffect = null;
+    if(c.iplBinauralEffectCreate(context, &audio_settings, &binaural_settings, &binaural) != c.IPL_STATUS_SUCCESS) {
+        return error.BinauralEffectCreationFailed;
+    }
+
+    // TODO: Load raw audio file
 }
